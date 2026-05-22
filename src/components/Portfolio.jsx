b9ce38e25051a05531416234093dc17e694d8002
Vinicius Lacerda <viniciuslacerda2@hotmail.com>
2026-05-21 22:02:50 -0300

    fix: Ajusta posição da logo no hero e troca imagem de adesivos no portfólio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,7 +6,7 @@ import vanImg from '../assets/images/van_envelopamento_new.png';
 import papelariaImg from '../assets/images/papelaria_new.png';
 import garrafasImg from '../assets/images/garrafas_new.png';
 import canecasImg from '../assets/images/canecas_new.png';
-import imasImg from '../assets/images/imas-095.jpg';
+import imasImg from '../assets/images/adesivos_geladeira_novo.png';
 import camisetasImg from '../assets/images/camisetas-093.jpg';
 import eventosImg from '../assets/images/eventos.png';
 import fotoImg from '../assets/images/foto_produtos.png';
